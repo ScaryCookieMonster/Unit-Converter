c2665b4c4ba77c7620ae8cb729c17fa39bf12208
ScaryCookieMonster <qangminhnguyen@gmail.com>
2025-06-26 14:15:58 +0930

    .

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 const inputEl = document.getElementById("input-el");
 const buttonEl = document.getElementById("button-el");
-const meterFeetEl = document.getElementById("meter/feet");
-const litersGallonsEl = document.getElementById("liters/gallons");
-const kilogramsPoundsEl = document.getElementById("kilograms/pounds");
-const value = Number(inputEl.value);
+const meterFeetEl = document.getElementById("lengthResults");
+const litersGallonsEl = document.getElementById("volumeResults");
+const kilogramsPoundsEl = document.getElementById("massResults");
 
 // Click Button
 buttonEl.addEventListener("click", function () {
-  lengthConverting(value)
-  volumeConverting(value)
-  massConverting(value)
+  let value = Number(inputEl.value);
+  const feet = (value * 3.28084).toFixed(3);
+  const meter = (value * 0.3048).toFixed(3);
+  const gallons = (value * 0.264172).toFixed(3);
+  const liters = (value * 3.78541).toFixed(3);
+  const pounds = (value * 2.20462).toFixed(3);
+  const kilograms = (value * 0.453592).toFixed(3);
+  lengthConverting(value, meter, feet);
+  volumeConverting(value, liters, gallons);
+  massConverting(value, kilograms, pounds);
 });
 // Meter/Feet Function
-function lengthConverting(units) {
-
+function lengthConverting(units, meter, feet) {
+  meterFeetEl.innerHTML = `<p>${units} meters = ${feet} feet | ${units} feet = ${meter} meters</p>`;
 }
 // Liters/Gallons Function
-function volumeConverting(units) {
+function volumeConverting(units, liters, gallons) {
+     litersGallonsEl.innerHTML = `<p>${units} liters = ${gallons} gallons | ${units} gallons = ${liters} liters</p>`;
 }
 // Kgs/Pounds Function
-function massConverting(units) {
-
+function massConverting(units, kilograms, pounds) {
+     kilogramsPoundsEl.innerHTML = `<p>${units} kilos = ${pounds} pounds | ${units} pounds = ${kilograms} kilos</p>`;
 }
