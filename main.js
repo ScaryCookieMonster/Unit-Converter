938d6bc0065fc60b985c6dcb74d69af77b6b31da
ScaryCookieMonster <qangminhnguyen@gmail.com>
2025-06-26 12:58:31 +0930

    .

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-const inputEl = document.getElementById('input-el')
-const buttonEl = document.getElementById('button-el')
+const inputEl = document.getElementById("input-el");
+const buttonEl = document.getElementById("button-el");
+const meterFeetEl = document.getElementById("meter/feet");
+const litersGallonsEl = document.getElementById("liters/gallons");
+const kilogramsPoundsEl = document.getElementById("kilograms/pounds");
+const value = Number(inputEl.value);
+
+// Click Button
+buttonEl.addEventListener("click", function () {
+  lengthConverting(value)
+  volumeConverting(value)
+  massConverting(value)
+});
+// Meter/Feet Function
+function lengthConverting(units) {
+
+}
+// Liters/Gallons Function
+function volumeConverting(units) {
+}
+// Kgs/Pounds Function
+function massConverting(units) {
+
+}
